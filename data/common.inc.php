7722ad3c95c0f30eb43991389298b4eccad4eec6
minmintemu <1433781574@qq.com>
2017-06-21 15:05:32 +0800

    这是名师堂书法网站

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'hktd28t.99aiji.net';
+$cfg_dbname = 'ailen99mysql';
+$cfg_dbuser = 'ailen99mysql';
+$cfg_dbpwd = 'A3281AA';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
